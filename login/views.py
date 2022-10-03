b1bec91d08daf42bad218c4cb918f47aeb3a3488
Fernando Baeza <fer@mini.local>
2022-10-03 12:44:13 +0200

    users

--- login/views.py
+++ login/views.py
@@ -1,11 +1,17 @@
+from pprint import pprint
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
 
 
 # Create your views here.
 def login(request):
-    #return HttpResponse("Hello Login")
-    return render(request, "loggin.html")
+    users = User.objects.all()
+    super = User.objects.filter(is_superuser=1)
+    pprint(super)
+    return render(request, "loggin.html", {"super":super, "users":users})
 
 def index(request):
     return HttpResponse("Hello Login")
